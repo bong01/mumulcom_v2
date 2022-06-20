65b466543819d8b664fc1a70048aacd05c366041
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-06-20 19:20:47 +0900

    Init: 프로젝트 초기 구성
    패키지 구성
    - api
    - domain
    - global.config
    - infra
    
    실행 환경 분리
    - dev
    - prod
    - test

---
+++ src/main/java/com/mumulcom/mumulcom_v2/MumulcomV2Application.java
@@ -0,0 +1,13 @@
+package com.mumulcom.mumulcom_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumulcomV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumulcomV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mumulcom/mumulcom_v2/MumulcomV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumulcom.mumulcom_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumulcomV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
